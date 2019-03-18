eb910232609cac75b9e19375d1bf64a1d35d0732
Alejandro Flores <alejandroflores@Alejandros-MacBook-Pro.local>
2019-03-18 17:47:06 -0600

    arreglados problemas de clienteResource.java

--- src/main/java/com/ayk/ereservation/vista/resources/ClienteResource.java
+++ src/main/java/com/ayk/ereservation/vista/resources/ClienteResource.java
@@ -3,8 +3,11 @@
  */
 package com.ayk.ereservation.vista.resources;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,10 +32,33 @@ public class ClienteResource {
 		this.clienteService = clienteService;	
 	}	
 	
+	@PostMapping
 	public ResponseEntity<Cliente> createCliente(@RequestBody ClienteVO clienteVo) {
 		Cliente cliente = new Cliente();
 		cliente.setNombreCli(clienteVo.getNombreCli());
+		cliente.setApellidoCli(clienteVo.getApellidoCli());
+		cliente.setDireccionCli(clienteVo .getDireccionCli());
+		cliente.setTelefonoCli(clienteVo.getTelefonoCli());
+		cliente.setEmailCli(clienteVo.getEmailCli());
 		
+		return new ResponseEntity<>(this.clienteService.create(cliente), HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/{identificacion}")
+	public ResponseEntity<Cliente> updateCliente(@PathVariable("identificacion") String identificacion, ClienteVO clienteVo) {
+		Cliente cliente = this.clienteService.findByIdentificacion(identificacion);
+		if (cliente == null) {
+			return new ResponseEntity<Cliente>(HttpStatus.NOT_FOUND);
+		}else {
+			
+		
+		cliente.setNombreCli(clienteVo.getNombreCli());
+		cliente.setApellidoCli(clienteVo.getApellidoCli());
+		cliente.setDireccionCli(clienteVo .getDireccionCli());
+		cliente.setTelefonoCli(clienteVo.getTelefonoCli());
+		cliente.setEmailCli(clienteVo.getEmailCli());
+		}
+		return new ResponseEntity<>(this.clienteService.update(cliente), HttpStatus.OK);
 	}
 
 	
